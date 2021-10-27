303453d1d36cd4bfa5467033282c2ba37dbd24e0
Joao Victor Martins <joaovictormr91@gmail.com>
2021-10-27 10:43:38 -0300

    Error handler creation and asyncwrapper

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ const express       = require('express');
 const app           = express();
 const tasksRoutes   = require('./routes/tasks');
 const connectDB     = require('./db/connect');
+const notFound      = require('./middleware/not-found');
+const errorHandlerMiddleware      = require('./middleware/error-handler');
+
 require('dotenv').config();
 
 
 app.use(express.json());
 app.use(express.static('./public'));
 app.use('/api/v1/tasks', tasksRoutes);
+app.use(errorHandlerMiddleware);
+app.use(notFound);
+const port = process.env.PORT || 3000;
 
 const start = async() => {
     try{
         await connectDB(process.env.MONGO_URI);
-        app.listen(3000, () => {
-            console.log("listening on port 3000...")
+        app.listen(port, () => {
+            console.log(`listening on port ${port}...`)
         });
     }catch (error) {
         console.log(error);
