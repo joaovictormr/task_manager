1d1b71256d26b27c28f231f470f41f03c96358e5
Joao Victor Martins <joaovictormr91@gmail.com>
2021-10-27 00:11:22 -0300

    CRUD for the tasks

---
+++ controllers/tasks.js
@@ -0,0 +1,70 @@
+const Task = require('../models/Task');
+
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json({tasks});
+    } catch(err) {
+        res.status(500).json({msg: err});
+    }
+};
+
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body);
+        res.status(201).json({task});
+    } catch(err) {
+        res.status(500).json({msg: err});
+    }
+};
+
+const getTask = async (req, res) => {
+    try {
+        const {id: taskId}  = req.params;
+        const task          = await Task.findOne({_id: taskId});
+        if(!task) {
+            return res.status(404).json({msg: `No task found with id ${taskId}`});
+        }
+        res.status(200).json({task});
+    } catch(err) {
+        res.status(500).json({msg: err}); 
+    }
+    
+};
+
+const updateTask = async (req, res) => {
+    try {
+        const {id: taskId}  = req.params;
+        const task          = await Task.findOneAndUpdate({_id: taskId}, req.body, {
+            new: true,
+            runValidators: true
+        });
+        if (!task) {
+            return res.status(404).json({msg: `No task found with id ${taskId}`});
+        }
+        res.status(200).json({task});
+    } catch(err) {
+        res.status(500).json({msg: err}); 
+    } 
+};
+
+const deleteTask = async (req, res) => {
+    try {
+        const {id: taskId}  = req.params;
+        const task          = await Task.findOneAndDelete({_id: taskId});
+        if (!task) {
+            return res.status(404).json({msg: `No task found with id ${taskId}`});
+        }
+        res.status(200).json({task});
+    } catch(err) {
+        res.status(500).json({msg: err});
+    }
+};
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+};
--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const express   = require('express');
-const app       = express();
+const express       = require('express');
+const app           = express();
+const tasksRoutes   = require('./routes/tasks');
+const connectDB     = require('./db/connect');
+require('dotenv').config();
 
 
+app.use(express.json());
+app.use(express.static('./public'));
+app.use('/api/v1/tasks', tasksRoutes);
 
-app.listen(3000, () => {
-    console.log("listening on port 3000...")
-});
+const start = async() => {
+    try{
+        await connectDB(process.env.MONGO_URI);
+        app.listen(3000, () => {
+            console.log("listening on port 3000...")
+        });
+    }catch (error) {
+        console.log(error);
+    }
+};
+
+start();
+
